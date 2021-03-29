9d105a2e82531896dc062563f4dd9036196cdbc6
qitas <qitas@qitas.cn>
2021-03-29 12:00:51 +0800

    update

---
+++ update.py
@@ -0,0 +1,5 @@
+import os
+
+class P14Platform(PlatformBase):
+    def configure_default_packages(self, variables, target):
+        return PlatformBase.configure_default_packages(self, variables,target)
