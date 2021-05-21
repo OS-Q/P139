c2729971caa0a2f5e07785e873e9083e29e3e4b2
qitas <qitas@qitas.cn>
2021-05-21 18:48:51 +0800

    init

---
+++ extend/main.py
@@ -0,0 +1,148 @@
+import sys
+from os.path import join
+from platform import system
+
+from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default,
+                            DefaultEnvironment)
+
+env = DefaultEnvironment()
+
+env.Replace(
+    AR="msp430-ar",
+    AS="msp430-as",
+    CC="msp430-gcc",
+    CXX="msp430-g++",
+    GDB="msp430-gdb",
+    OBJCOPY="msp430-objcopy",
+    RANLIB="msp430-ranlib",
+    SIZETOOL="msp430-size",
+    LINK="$CC",
+
+    ARFLAGS=["rc"],
+
+    PIODEBUGFLAGS=["-O0", "-g3", "-ggdb", "-gdwarf-2"],
+
+    SIZEPROGREGEXP=r"^(?:\.text|\.data|\.rodata|\.vectors)\s+([0-9]+).*",
+    SIZEDATAREGEXP=r"^(?:\.data|\.bss|\.noinit)\s+(\d+).*",
+    SIZECHECKCMD="$SIZETOOL -A -d $SOURCES",
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    UPLOADER="mspdebug",
+    UPLOADERFLAGS=[
+        "$UPLOAD_PROTOCOL",
+        "--force-reset"
+    ],
+    UPLOADCMD='$UPLOADER $UPLOADERFLAGS "prog $SOURCES"',
+
+    PROGSUFFIX=".elf"
+)
+
+env.Append(
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-Os",
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-mmcu=$BOARD_MCU"
+    ],
+
+    CXXFLAGS=[
+        "-fno-exceptions",
+        "-fno-threadsafe-statics"
+    ],
+
+    CPPDEFINES=[
+        ("F_CPU", "$BOARD_F_CPU")
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-mmcu=$BOARD_MCU",
+        "-Wl,-gc-sections,-u,main"
+    ],
+
+    LIBS=["m"],
+
+    BUILDERS=dict(
+        ElfToHex=Builder(
+            action=env.VerboseAction(" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"
+            ]), "Building $TARGET"),
+            suffix=".hex"
+        )
+    )
+)
+
+# copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
+env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
+
+
+# Allow user to override via pre:script
+if env.get("PROGNAME", "program") == "program":
+    env.Replace(PROGNAME="firmware")
+
+if "energia" in env.get("PIOFRAMEWORK", []):
+    sys.stderr.write(
+        "WARNING!!! Using of `framework = energia` in `platformio.ini` is "
+        "deprecated. Please replace with `framework = arduino`.\n")
+    env.Replace(PIOFRAMEWORK=["arduino"])
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = None
+if "nobuild" in COMMAND_LINE_TARGETS:
+    target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
+    target_firm = join("$BUILD_DIR", "${PROGNAME}.hex")
+else:
+    target_elf = env.BuildProgram()
+    target_firm = env.ElfToHex(join("$BUILD_DIR", "${PROGNAME}"), target_elf)
+
+AlwaysBuild(env.Alias("nobuild", target_firm))
+target_buildprog = env.Alias("buildprog", target_firm, target_firm)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias(
+    "size", target_elf,
+    env.VerboseAction("$SIZEPRINTCMD", "Calculating size $SOURCE"))
+AlwaysBuild(target_size)
+
+#
+# Target: Upload firmware
+#
+
+upload_target = target_firm
+if env.subst("$UPLOAD_PROTOCOL") == "dslite":
+    env.Replace(
+        UPLOADER=join(env.PioPlatform().get_package_dir(
+            "tool-dslite") or "", "DebugServer", "bin", "DSLite"),
+        UPLOADERFLAGS=[
+            "load", "-c",
+            join(env.PioPlatform().get_package_dir("tool-dslite") or "",
+                 "%s.ccxml" % env.BoardConfig().get("build.variant")), "-f"
+        ],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES"
+    )
+    upload_target = target_elf
+    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+target_upload = env.Alias("upload", upload_target,
+                          env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE"))
+AlwaysBuild(target_upload)
+
+#
+# Default targets
+#
+
+Default([target_buildprog, target_size])
